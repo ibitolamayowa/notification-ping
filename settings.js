404b24a90d760f4d9ad1915e34b626fab485c464
John Pollard <johnp@bravelocation.com>
2017-06-12 10:58:39 +0100

    Added Mickledore site to monitors

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 var settings = {
-    urls: [ "https://bravelocation.com", "http://josiemccoy.co.uk", "http://yeltzland.net", "http://yeltz.co.uk/0/"]
+    urls: [ "https://bravelocation.com", "http://josiemccoy.co.uk", "http://yeltzland.net", "http://yeltz.co.uk/0/", "https://docs.mickledore.co.uk"]
 };
 
 module.exports = settings;
