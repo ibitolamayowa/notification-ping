ed6911d468244d45dc7d57564b3e6eb1be436b93
John Pollard <johnp@bravelocation.com>
2017-10-21 09:41:04 +0100

    Added AWITC URL

--- settings.js
+++ settings.js
@@ -1,5 +1,12 @@
 var settings = {
-    urls: [ "https://bravelocation.com", "http://josiemccoy.co.uk", "http://yeltzland.net", "http://yeltz.co.uk/0/", "https://docs.mickledore.co.uk"]
+    urls: [ 
+        "https://bravelocation.com", 
+        "http://josiemccoy.co.uk", 
+        "http://yeltzland.net", 
+        "http://yeltz.co.uk/0/", 
+        "https://docs.mickledore.co.uk",
+        "https://docs.awalkinthecountry.com"
+    ]
 };
 
 module.exports = settings;
