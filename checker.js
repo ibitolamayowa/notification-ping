caed5c04b680f3b9eda4226fc5df6e18a168795e
John Pollard <johnp@bravelocation.com>
2018-04-30 15:46:21 +0100

    Refactored code to put functionality in module

--- checker.js
+++ checker.js
@@ -1,58 +1,10 @@
 var settings = require("./settings");
-var request = require("request");
-var IncomingWebhook = require('@slack/client').IncomingWebhook;
-
-// Setup Slack
-var url = process.env.SLACK_WEBHOOK_URL || '';
-var webhook = new IncomingWebhook(url);
-
-// Function which tries hitting the given url
-function checkWebsite(url, callback) {
-    request(
-        {uri: url, method: "GET", timeout: 30000, followRedirect: false}, 
-        function(error, response) {
-            var success = false;
-            var errorCode = 999;
-    
-            if (response) {
-                errorCode = response.statusCode;
-
-                if (error == null && response.statusCode == 200) {
-                    success = true;
-                }
-            }
-
-            console.log(url + ": " + success + " (" + errorCode + ")");
-            callback(url, success, errorCode, error);
-        });  
-};
-
-// Function that sends error message via Slack
-function sendErrorMessage(url, errorCode, error) {
-    var message = "Site down: " + url + " (" + errorCode + "): " + error;
-    webhook.send(message, function(err, res) {
-        if (err) {
-            console.log('Error:', err);
-        } else {
-            console.log('Message sent: ', res);
-        }
-    });
-}
+var WebPingToSlackService = require("./web-ping-to-slack");
 
 settings.urls.forEach(function(url) {
-    checkWebsite(url, function(url, success, errorCode, error) {
+    WebPingToSlackService.checkWebsite(url, function(url, success, errorCode, error) {
         if (success == false) {
-            sendErrorMessage(url, errorCode, error);
+            WebPingToSlackService.sendErrorMessage(url, errorCode, error);
         }
     });
 });
-
-
-
-
-
-
-
-
-
-
---
+++ web-ping-to-slack.js
@@ -0,0 +1,44 @@
+var settings = require("./settings");
+var request = require("request");
+var IncomingWebhook = require('@slack/client').IncomingWebhook;
+
+var WebPingToSlackService = function () {}
+
+// Setup Slack
+var url = process.env.SLACK_WEBHOOK_URL || '';
+var webhook = new IncomingWebhook(url);
+
+// Function which tries hitting the given url
+WebPingToSlackService.checkWebsite = function (url, callback) {
+    request(
+        {uri: url, method: "GET", timeout: 30000, followRedirect: false}, 
+        function(error, response) {
+            var success = false;
+            var errorCode = 999;
+    
+            if (response) {
+                errorCode = response.statusCode;
+
+                if (error == null && response.statusCode == 200) {
+                    success = true;
+                }
+            }
+
+            console.log(url + ": " + success + " (" + errorCode + ")");
+            callback(url, success, errorCode, error);
+        });  
+};
+
+// Function that sends error message via Slack
+WebPingToSlackService.sendErrorMessage = function(url, errorCode, error) {
+    var message = "Site down: " + url + " (" + errorCode + "): " + error;
+    webhook.send(message, function(err, res) {
+        if (err) {
+            console.log('Error:', err);
+        } else {
+            console.log('Message sent: ', res);
+        }
+    });
+}
+
+module.exports = WebPingToSlackService;
